ac9fb763863643202b14cba0bf804e07020ee60b
kunq <Kun_Qian@msn.com>
2016-01-17 22:19:31 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+  if num2 != 0 and num2 != nil
+     return num1/num2
+  else
+    puts "Can't not devided by 0 or nil" 
+  end
+
 end
 
 def assign_variable(value)
-  
+  return(value)  
 end
 
-def argue
-  
+def argue(phrase)
+  return phrase 
 end
 
-def greeting
-
+def greeting(phrase, name)
+  return (phrase + ' ' + name)
 end
 
 def return_a_value
-
+  return 'Nice'
 end
 
 def last_evaluated_value
-
+   return 'expert'
 end
 
-def pizza_party
-
+def pizza_party(pizza="cheese")
+  return pizza
 end
